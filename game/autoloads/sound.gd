b5f75edf5875c058ad684edcea6061b0ea2157cc
Fio <leukert.fio@gmail.com>
2024-12-13 18:08:29 +0100

    add diisis

---
+++ game/autoloads/sound.gd
@@ -0,0 +1,106 @@
+extends Node
+
+var bgm_key := ""
+
+var audio_players := []
+var main_audio_player : AudioStreamPlayer
+
+
+func serialize() -> Dictionary:
+	var data := {}
+	
+	data["bgm_key"] = bgm_key
+	if main_audio_player:
+		data["playback_position"] = main_audio_player.get_playback_position()
+	else:
+		data["playback_position"] = 0
+	
+	return data
+
+func deserialize(data:Dictionary):
+	play_bgm(data.get("bgm_key", ""), 0.0, data.get("playback_position", 0.0))
+
+func set_audio_player_volume(volume:float):
+	main_audio_player.volume_db = linear_to_db(volume)
+	for player : AudioStreamPlayer in audio_players:
+		if player == main_audio_player:
+			continue
+		player.volume_db = linear_to_db((1.0 - volume) / (audio_players.size() - 1))
+		if db_to_linear(player.volume_db) <= 0.0:
+			audio_players.erase(player)
+			player.queue_free()
+
+func play_sfx(sfx:String):
+	var player := AudioStreamPlayer.new()
+	player.stream = load(str(CONST.SFX_ROOT, CONST.get(str("SFX_", sfx.to_upper()))))
+	player.set_bus("SFX")
+	add_child(player)
+	player.pitch_scale = randf_range(0.85, 1.0 / 0.85)
+	player.play()
+	player.finished.connect(player.queue_free)
+
+func play_bgm(bgm:String, fade_in:=0.0, from:=0.0):
+	if bgm_key == bgm:
+		return
+	
+	if bgm == "none" or bgm == "null":
+		return
+	
+	var preserve_sex : bool
+	var sex_position : float
+	if bgm.begins_with("sex_one") and bgm_key.begins_with("sex_one"):
+		preserve_sex = true
+		sex_position = main_audio_player.get_playback_position()
+	if bgm.begins_with("sex_anhedonia") and bgm_key.begins_with("sex_anhedonia"):
+		preserve_sex = true
+		sex_position = main_audio_player.get_playback_position()
+	bgm_key = bgm 
+	
+	var music_player = AudioStreamPlayer.new()
+	music_player.connect("tree_exiting", audio_players.erase.bind(music_player))
+	main_audio_player = music_player
+	
+	var music_path := str(CONST.MUSIC_ROOT, CONST.get(str("MUSIC_", bgm_key.to_upper())))
+	if not ResourceLoader.exists(music_path):
+		push_error(str(music_path, " doesn't exist with key \"", bgm_key, "\""))
+		return
+	music_player.stream = load(music_path)
+	music_player.volume_db = -80
+	music_player.set_bus("Music")
+	
+	if fade_in > 0.0:
+		var t = create_tween()
+		t.tween_method(
+			set_audio_player_volume,
+			0.0,
+			1.0,
+			fade_in
+			)
+		#for player in audio_players:
+			#t.set_parallel()
+			#t.tween_method(
+			#set_audio_player_volume,
+			#db_to_linear(player.volume_db),
+			#0.0,
+			#fade_in
+			#)
+			#t.tween_callback(player.queue_free)
+	else:
+		while not audio_players.is_empty():
+			var player : AudioStreamPlayer = audio_players.pop_front()
+			player.queue_free()
+		music_player.volume_db = linear_to_db(Options.music_volume)
+	
+	if preserve_sex:
+		from = sex_position
+	
+	audio_players.append(music_player)
+	add_child(music_player)
+	music_player.play(from)
+	
+
+func fade_out_bgm(fade_out_time:float):
+	if not main_audio_player:
+		return
+	var t = create_tween()
+	t.tween_property(main_audio_player, "volume_db", linear_to_db(0.0001), fade_out_time)
---
+++ game/stages/main_menu_stage.gd
@@ -0,0 +1,43 @@
+extends Control
+
+signal start_game()
+signal load_game()
+
+func _ready() -> void:
+	#Sound.play_bgm("main_menu")
+	find_child("QuitButton").visible = not OS.has_feature("web")
+	find_child("LoadButton").visible = Options.does_savegame_exist()
+	
+	find_child("LoadButton").text = str("Load (", int(Parser.get_game_progress_from_file(Options.SAVEGAME_PATH) * 100), "%)")
+
+
+func _gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if GameWorld.stage_root.get_node("ScreenContainer").get_child_count() == 0:
+			GameWorld.stage_root.set_screen("")
+		else:
+			GameWorld.stage_root.set_screen(CONST.SCREEN_OPTIONS)
+
+func _on_quit_button_pressed() -> void:
+	Options.save_prefs()
+	get_tree().quit()
+
+
+func _on_options_button_pressed() -> void:
+	GameWorld.stage_root.set_screen(CONST.SCREEN_OPTIONS)
+
+
+func _on_credits_button_pressed() -> void:
+	GameWorld.stage_root.set_screen(CONST.SCREEN_CREDITS)
+
+
+func _on_cw_button_pressed() -> void:
+	GameWorld.stage_root.set_screen(CONST.SCREEN_CONTENT_WARNING)
+
+
+func _on_discord_button_pressed() -> void:
+	OS.shell_open("https://discord.gg/jPU4RvmTvP")
+
+
+func _on_git_hub_button_pressed() -> void:
+	OS.shell_open("https://github.com/SnekOfSpice/dialog-editor")
